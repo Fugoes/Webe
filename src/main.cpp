d2c072e129361d97e529aaaf6971c9b55b1e1a83
Fugoes <fugoes.qa@gmail.com>
2017-08-11 16:37:53 +0800

    new format

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "utils.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    IF_NEGATIVE_EXIT(-1);
     return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -5,12 +5,11 @@
 #include <cstdlib>
 #include <cstdio>
 
-#define IF_NEGATIVE_EXIT(EXP)                   \
-    if (EXP < 0) {                              \
-        fprintf(stderr, "%s:%d",                \
-                __FILE__, __LINE__);            \
-        perror(#EXP);                           \
-        exit(1);                                \
+#define IF_NEGATIVE_EXIT(EXP)                           \
+    if (EXP < 0) {                                      \
+        fprintf(stderr, "%s:%d\n", __FILE__, __LINE__); \
+        fprintf(stderr, "--> %s\n", #EXP);              \
+        exit(1);                                        \
     }
 
 #endif //WEBE_UTILS_H
